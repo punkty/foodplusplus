35d7e0090fe3ba2b9b5fdbde5c1254b6a1ef89ae
ty <iamtylerwarren@gmail.com>
2016-11-19 23:12:38 -0800

    created models, set up views and urls

--- apps/donate/models.py
+++ apps/donate/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
-
+from ..login.models import User
 from django.db import models
 
-# Create your models here.
+
+class ItemManager(models.Manager):
+    def add_offer:
+        pass
+    
+    
+        
+        
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    foodbank = models.ForeignKey(User,related_name='item_requests')
+    donor = models.ForeignKey(User,related_name='item_offers')
+    active = models.BooleanField(initial=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = ItemManager()
--- apps/donate/views.py
+++ apps/donate/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    #will display a dasboard depending on which type of user is logged in
+    pass
+
+def add_offer(request):
+    #creates a offer from a donter
+    pass
+
+def add_request(request):
+    #creates a request for a foodbank
+    pass
+
+def fulfill(request, item_id):
+    #updates a donater foreign key to item
+    pass
+
+def claim(request, item_id):
+    # allows a foodbank to claim offered items
+    pass
+
+def recieved(request, item_id):
+    #updates item's active state and completes donation
+    pass
+
+def cancel(request, item_id):
+    #allows foodbank to reject a fulfillment
+    pass
+
+def show_donor(request, user_id):
+    #renders donors page featuring completed donations and current offers
+    pass
+
+def show_foodbank(request, user_id):
+    #renders foodbank page featuring current items requested and location?
+    pass
+
+def show_all(request):
+    #shows queries on whats available based on user type
+    pass
+
