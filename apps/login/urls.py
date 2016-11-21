957c3eadbf16f03fa06c7782d2719a6ff9b2f900
ty <iamtylerwarren@gmail.com>
2016-11-21 10:16:50 -0800

    top donations query and display on login page

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login_reg$', views.login_reg, name='login_reg'),
-    url(r'^logout$', views.logout, name='logout')
+    url(r'^logout$', views.logout, name='logout'),
+    url(r'^donate_hack$', views.donate_hack, name='donate_hack')
 ]
