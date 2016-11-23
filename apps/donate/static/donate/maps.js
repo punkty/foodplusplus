16017493003a7827e211bc42cfa36762ee1f5a04
ty <iamtylerwarren@gmail.com>
2016-11-22 19:34:46 -0800

    fixed food bank option box

--- apps/donate/static/donate/maps.js
+++ apps/donate/static/donate/maps.js
@@ -8,7 +8,7 @@ function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 47.734145, lng: -122.224433 },
-        zoom: 13,
+        zoom: 9,
         mapTypeControl: false
     });
     // This autocomplete is for use in the search within time entry box.
